6761ddfaab0d9aff92042f820316b56c14b73972
Chamomile <dltldms5271@gmail.com>
2023-10-12 16:26:12 +0900

    04

--- main.c
+++ main.c
@@ -1,41 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int sumTwo(int a, int b) //반환형 함수 (매개변수) 선언 
+/*void square( int a )
 {
-    int c = a + b;
-    return c;
-} 
-
-int square(int n) 
-{
-    return n*n;
+     a = a*a;
 }
 
-int get_max(int x, int y) //둘중에 큰 거 출력 
+int main()
 {
-    if (x > y)
-       return x;
-    else  //else 생략 가능. x가 크면 x가 출력될 테니. 
-       return y;
+    int a = 2;
+    square(a);
+    printf("a=%i\n", a); */
+    
+int square( int a )
+{
+    return(a*a);
 }
 
-int main(int argc, char*argv[])
+int main()
 {
-    int a, b;
-    int n;
-    int x, y;
-    
-    a = 15;
-    b = 4; 
-    printf("sumTwo result : %i\n", sumTwo(a, b));
-    
-    n = 4;
-    printf("square result : %i\n", square(n));
-    
-    x = 13;
-    y = 48;
-    printf("get_max result : %i\n", get_max(x, y));
+    int a = 2;
+    a = square(a);
+    printf("a=%i\n",a); 
+ 
      
   system("PAUSE");	
   return 0;
